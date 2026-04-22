83126527e6cac57a916f24aff99ed319d4045c92
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-04-23 00:34:59 +0600

    Interactivity - Group Hover

--- src/topics/Interactivity.jsx
+++ src/topics/Interactivity.jsx
@@ -10,6 +10,13 @@ const Interactivity = () => {
         <button className=' border p-1 rounded-xl bg-blue-500 m-20 cursor-pointer hover:bg-blue-400 focus:bg-amber-400 '>Login</button>
 
         <button className=' border p-1 rounded-xl bg-blue-500 m-20 cursor-pointer hover:bg-blue-400 focus:bg-green-500 active:bg-red-400 '>Login</button>
+
+        <div>
+            <a href="" className=' group'>
+                <h1 className=' group-hover:bg-amber-200'>heading click</h1>
+                <p className=' group-hover:text-4xl'>para click</p>
+            </a>
+        </div>
       
     </div>
   )
