0d2254634ca5c496cd8d33c2b1815b322ef19bc3
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-04-24 00:35:06 +0600

    Flex topic complete

--- src/topics/Flex.jsx
+++ src/topics/Flex.jsx
@@ -12,13 +12,36 @@ const Flex = () => {
         <div className="size-20 bg-green-600">6</div>
        
       </div> */}
-      <div className=' bg-gray-900  size-full flex flex-row-reverse justify-center items-center'>
+      {/* <div className=' bg-gray-900  size-full flex flex-wrap content-between'>
         <div className="size-20 bg-green-100">1</div>
+        <div className="size-20 bg-blue-100">1</div>
         <div className="size-20 bg-green-200">2</div>
+        <div className="size-20 bg-blue-300">3</div>
         <div className="size-20 bg-green-300">3</div>
+        <div className="size-20 bg-blue-500">5</div>
         <div className="size-20 bg-green-400">4</div>
+        <div className="size-20 bg-blue-200">2</div>
         <div className="size-20 bg-green-500">5</div>
+        <div className="size-20 bg-blue-400">4</div>
         <div className="size-20 bg-green-600">6</div>
+        <div className="size-20 bg-blue-600">6</div>
+       
+      </div> */}
+
+      {/* jodi responsive banate chai. */}
+      <div className=' bg-gray-900  size-full flex flex-col flex-wrap content-start md:flex-row'>
+        <div className="size-20 bg-green-100">1</div>
+        <div className="size-20 bg-blue-100">1</div>
+        <div className="size-20 bg-green-200">2</div>
+        <div className="size-20 bg-blue-300">3</div>
+        <div className="size-20 bg-green-300">3</div>
+        <div className="size-20 bg-blue-500">5</div>
+        <div className="size-20 bg-green-400">4</div>
+        <div className="size-20 bg-blue-200">2</div>
+        <div className="size-20 bg-green-500">5</div>
+        <div className="size-20 bg-blue-400">4</div>
+        <div className="size-20 bg-green-600">6</div>
+        <div className="size-20 bg-blue-600">6</div>
        
       </div>
     </div>
