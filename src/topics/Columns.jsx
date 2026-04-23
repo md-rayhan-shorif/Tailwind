7a8a767ebda263e0b3d66806610c17635e546501
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-04-23 23:56:10 +0600

    Automate Columns Set

--- src/topics/Columns.jsx
+++ src/topics/Columns.jsx
@@ -5,7 +5,7 @@ const Columns = () => {
   return (
     <div className=' h-screen'>
 
-        <div className=' columns-4'>
+        {/* <div className=' columns-4'>
 
 
         <h1 className=' bg-green-500'>Heading 1</h1>
@@ -16,9 +16,9 @@ const Columns = () => {
         <h1 className=' bg-red-400'>Heading 6</h1>
         <h1 className=' bg-red-400'>Heading 6</h1>
       
-        </div>
+        </div> */}
         {/* jodi manual set korte chai koto gulo image rakhbo screen e. */}
-        <div className=' columns-3'>
+        {/* <div className=' columns-3'>
             <img src={photo} alt="" />
             <img src={photo} alt="" />
             <img src={photo} alt="" />
@@ -26,9 +26,25 @@ const Columns = () => {
             <img src={photo} alt="" />
             <img src={photo} alt="" />
             <img src={photo} alt="" />
-        </div>
+        </div> */}
     {/* Responsive condition diye columns set kora */}
-        <div className=' columns-1 lg:columns-4 md:columns-3 sm:columns-2 '>
+        {/* <div className=' columns-1 lg:columns-4 md:columns-3 sm:columns-2 '>
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+            <img src={photo} alt="" />
+        </div> */}
+        <div className=' columns-xs gap-0 '>
             <img src={photo} alt="" />
             <img src={photo} alt="" />
             <img src={photo} alt="" />
@@ -44,6 +60,9 @@ const Columns = () => {
             <img src={photo} alt="" />
             <img src={photo} alt="" />
         </div>
+
+
+
     </div>
   )
 }
